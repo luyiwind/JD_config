183768c3979da6f4ee7b7c0469f49b64aff3285d
luyiwind <70989874+luyiwind@users.noreply.github.com>
2021-02-28 14:04:12 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,23 +8,22 @@ done
 
 ##############################作者昵称（必填）##############################
 # 使用空格隔开
-author_list="qq34347476 ZCY01 i-chenzhe"
+author_list="qq34347476 i-chenzhe moposmall"
 
 ##############################作者脚本地址URL（必填）##############################
 # 例如：https://raw.githubusercontent.com/whyour/hundun/master/quanx/jx_nc.js
 # 1.从作者库中随意挑选一个脚本地址，每个作者的地址添加一个即可，无须重复添加
 # 2.将地址最后的 “脚本名称+后缀” 剪切到下一个变量里（my_scripts_list_xxx）
 scripts_base_url_1=https://gitee.com/qq34347476/quantumult-x/raw/master/
-scripts_base_url_2=https://raw.githubusercontent.com/ZCY01/daily_scripts/main/jd/
-scripts_base_url_3=https://raw.githubusercontent.com/i-chenzhe/qx/main/
+scripts_base_url_2=https://raw.githubusercontent.com/i-chenzhe/qx/main/
+scripts_base_url_3=https://raw.githubusercontent.com/moposmall/Script/main/Me/
 
 
 ##############################作者脚本名称（必填）##############################
 # 将相应作者的脚本填写到以下变量中
-my_scripts_list_1="format_share_jd_code.js"
-my_scripts_list_2="jd_priceProtect.js jd_try.js"
-my_scripts_list_3="jd_asus_iqiyi.js jd_fanslove.js jd_jump-jump.js"
-
+my_scripts_list_1="format_share_jd_code.js whyour_jx_cfdtx.js"
+my_scripts_list_2="jd_asus_iqiyi.js jd_fanslove.js jd_jump-jump.js jd_entertainment.js jd_shake.js"
+my_scripts_list_3="jx_cfd.js"
 
 ##############################随机函数##########################################
 rand(){
