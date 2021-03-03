007119481bad21c3100923e71f6deb25faa930d5
luyiwind <70989874+luyiwind@users.noreply.github.com>
2021-03-03 12:24:12 +0800

    Create append.sh

---
+++ append.sh
@@ -0,0 +1,11 @@
+DiyUpdate
+
+wget -q https://raw.githubusercontent.com/luyiwind/JD_config/main/diy.sh -O $BASE/config/myself.sh
+if [ $? -eq 0 ];then
+  echo -e "\033[37mdiy脚本同步成功... \033[0m"
+  echo -e ''
+else
+  echo -e "\033[37mdiy脚本同步失败... \033[0m"
+  echo -e ''
+fi
+bash $BASE/config/myself.sh
