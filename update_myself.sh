7cff82f08276a7b3b70b0154d976008ceca3fbc7
luyiwind <70989874+luyiwind@users.noreply.github.com>
2021-03-03 12:34:44 +0800

    Create update_myself.sh

---
+++ update_myself.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+wget -q https://raw.githubusercontent.com/SuperManito/JD-FreeFuck/main/manual-update.sh /opt/jd/manual-update.sh
+if [ $? -eq 0 ];then
+  echo -e "\033[37mManual-update脚本同步成功... \033[0m"
+  echo -e ''
+else
+  echo -e "\033[37mManual-update脚本同步失败... \033[0m"
+  echo -e ''
+  exit 1
+fi
+
+wget -q https://raw.githubusercontent.com/luyiwind/JD_config/main/append.sh /opt/jd/append.sh
+if [ $? -eq 0 ];then
+  echo -e "\033[37m追加脚本同步成功... \033[0m"
+  echo -e ''
+  cat /opt/jd/append.sh >> /opt/jd/manual-update.sh
+  echo -e "\033[37mManual-update脚本追加成功... \033[0m"
+  echo -e ''
+else
+  echo -e "\033[37m追加脚本同步失败... \033[0m"
+  echo -e ''
+  exit 1
+fi
