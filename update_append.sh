7810ee43bb3bbf63788c31dcc9a1814842a2ee74
luyiwind <70989874+luyiwind@users.noreply.github.com>
2021-03-04 10:19:42 +0800

    Update update_append.sh

--- update_append.sh
+++ update_append.sh
@@ -4,10 +4,10 @@ wget -q https://raw.githubusercontent.com/luyiwind/JD_config/main/update_append.
 
 wget -q https://raw.githubusercontent.com/SuperManito/JD-FreeFuck/source/git_pull.sh -O /opt/jd/git_pull.sh
 if [ $? -eq 0 ];then
-  echo -e "\033[37mManual-update脚本同步成功... \033[0m"
+  echo -e "\033[37mgit_pull脚本同步成功... \033[0m"
   echo -e ''
 else
-  echo -e "\033[37mManual-update脚本同步失败... \033[0m"
+  echo -e "\033[37mgit_pull脚本同步失败... \033[0m"
   echo -e ''
   exit 1
 fi
@@ -17,10 +17,10 @@ if [ $? -eq 0 ];then
   echo -e "\033[37m追加脚本同步成功... \033[0m"
   echo -e ''
   cat /opt/jd/append.sh >> /opt/jd/git_pull.sh
-  echo -e "\033[37mManual-update脚本追加成功... \033[0m"
+  echo -e "\033[37mgit_pull脚本追加成功... \033[0m"
   echo -e ''
 else
-  echo -e "\033[37m追加脚本同步失败... \033[0m"
+  echo -e "\033[37mgit_pull追加脚本同步失败... \033[0m"
   echo -e ''
   exit 1
 fi
