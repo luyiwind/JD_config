bd0dc3e052c4ea389ad9c794315e41ba7c41c3b9
luyiwind <70989874+luyiwind@users.noreply.github.com>
2021-03-10 16:16:15 -0600

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -4,7 +4,7 @@
 ShellDir=/opt/jd
 ##############################  作  者  昵  称  （必填）  ##############################
 # 使用空格隔开
-author_list="qq34347476 moposmall yangtingxiao"
+author_list="qq34347476 moposmall yangtingxiao luyiwind"
 
 ## 添加更多作者昵称（必填）示例：author_list="i-chenzhe whyour testuser"  直接追加，不要新定义变量
 
@@ -15,7 +15,7 @@ author_list="qq34347476 moposmall yangtingxiao"
 scripts_base_url_1=https://gitee.com/qq34347476/quantumult-x/raw/master/ 
 scripts_base_url_2=https://raw.githubusercontent.com/moposmall/Script/main/Me/ 
 scripts_base_url_3=https://raw.githubusercontent.com/yangtingxiao/QuantumultX/master/scripts/jd/
-scripts_base_url_4=https://jdsharedresourcescdn.azureedge.net/jdresource/
+scripts_base_url_4=https://raw.githubusercontent.com/i-chenzhe/qx/main/
 ## 添加更多脚本地址URL示例：scripts_base_url_3=https://raw.githubusercontent.com/SuperManito/JD-FreeFuck/master/
 
 ##############################  作  者  脚  本  名  称  （必填）  ##############################
@@ -23,7 +23,7 @@ scripts_base_url_4=https://jdsharedresourcescdn.azureedge.net/jdresource/
 my_scripts_list_1="format_share_jd_code.js whyour_jx_cfdtx.js"
 my_scripts_list_2="jx_cfd_exchange.js"
 my_scripts_list_3="jd_baiTiao.js"
-#my_scripts_list_4="jd_live_redrain.js"
+#my_scripts_list_4="z_superDay.js"
 
 ##############################  随  机  函  数  ##########################################
 rand() {
