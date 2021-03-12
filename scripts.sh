48b5652e44fc0604fcdee2916a0214757ec65a6b
luyiwind <70989874+luyiwind@users.noreply.github.com>
2021-03-12 10:24:07 +0800

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -15,7 +15,7 @@ author_list="qq34347476 moposmall yangtingxiao luyiwind"
 scripts_base_url_1=https://gitee.com/qq34347476/quantumult-x/raw/master/ 
 scripts_base_url_2=https://raw.githubusercontent.com/moposmall/Script/main/Me/ 
 scripts_base_url_3=https://raw.githubusercontent.com/yangtingxiao/QuantumultX/master/scripts/jd/
-#scripts_base_url_4=https://raw.githubusercontent.com/i-chenzhe/qx/main/
+scripts_base_url_4=https://raw.githubusercontent.com/i-chenzhe/qx/main/
 ## 添加更多脚本地址URL示例：scripts_base_url_3=https://raw.githubusercontent.com/SuperManito/JD-FreeFuck/master/
 
 ##############################  作  者  脚  本  名  称  （必填）  ##############################
@@ -23,7 +23,7 @@ scripts_base_url_3=https://raw.githubusercontent.com/yangtingxiao/QuantumultX/ma
 my_scripts_list_1="format_share_jd_code.js whyour_jx_cfdtx.js"
 my_scripts_list_2="jx_cfd_exchange.js"
 my_scripts_list_3="jd_baiTiao.js"
-#my_scripts_list_4="z_superDay.js"
+my_scripts_list_4="z_superDay.js"
 
 ##############################  随  机  函  数  ##########################################
 rand() {
